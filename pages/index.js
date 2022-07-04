3a2404f9edc297d6bff6444976c3668a227ef4c2
Placido Hoff <67982091+placidohoff@users.noreply.github.com>
2022-07-04 14:51:07 -0400

    adding hotjar

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,17 @@ export default function Home() {
         <p className={styles.mainText}>Providing Commercial and Residential Services For Over 20 Years</p>
         <p className={styles.subText}>Licensed Master Electrician For Work In:</p>
         <p className={styles.subText}>RI , MA , CT , NH</p>
+
+        {
+          (function (h, o, t, j, a, r) {
+            h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+            h._hjSettings = { hjid: 3051363, hjsv: 6 };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script'); r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+        }
         <Link href='/services'>
           <div className={styles.buttonWrap}><p className={styles.mainButton}>See What We Can Do For You!</p></div>
         </Link>
